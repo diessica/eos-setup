b5c1077755c168b9fff2e187107a1361eb41cba7
Diéssica Gurskas <diessica.gurskas@gmail.com>
2014-01-19 10:34:20 -0200

    Create installer (incomplete)

---
+++ softwares.sh
@@ -0,0 +1,14 @@
+# Prompting
+read -p "What package manager do you use? (E.g. apt-get, aptitude) | " packageManager
+
+# Development
+$packageManager install vim
+
+# Browser
+$packageManager install chromium-browser
+
+# Media
+$packageManager install rhythmbox
+
+# Tools
+$packageManager install tilda
