e36813af5eb97764eb954112bfbbc662ee3056c8
Diéssica Gurskas <diessicode@gmail.com>
2014-06-08 17:26:44 -0300

    Add conditional before installing

--- dot/npm/pack.sh
+++ dot/npm/pack.sh
@@ -6,7 +6,15 @@ run_npm() {
     
     # list of npm packages
     local packages="grunt-cli gulp bower csslint csscomb stylus imageoptim-cli gh jade hexo jasmine-node yslow shelljs uglify-js jshint browser-sync markdown"
+    
+    # if doesn't exist yet, then install
     npm install $packages -g
+    if which ${packages[@]} &> /dev/null; then
+    	msg_ok "${packages[@]}"
+    else
+    	msg_run "${packages[@]}" "sudo npm install -g ${packages[@]}"
+    	sudo npm install -g ${packages[@]}
+    fi
 
     [[ $? ]] && e_success "Done"
     
